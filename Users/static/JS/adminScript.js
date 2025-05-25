4ba156b36cd282f4af880ae38c661e26bf4bbca8
Santiago Medina López <smedina@iegabo.edu.co>
2025-05-25 16:21:00 -0500

    Refactor admin styles and scripts: update class names for consistency, enhance form visibility toggling, and clean up unused code in admin interface.

--- Users/static/JS/adminScript.js
+++ Users/static/JS/adminScript.js
@@ -1,5 +1,3 @@
-const select = document.getElementById('open-dialog');
-select.addEventListener('click', validateGroup)
 const label_candidate = document.querySelector('.create-candidate-label');
 const label_subuser = document.querySelector('.create-subuser-label');
 const create_candidate = document.getElementById('create-candidate');
@@ -12,23 +10,23 @@ label_subuser.addEventListener('click', function (){
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const subusersTable = document.querySelector('.subusers-table__container');
+    const resultados = document.querySelector('.resultados');
     const closeFormGroup = document.querySelector(".close-formGroup");
     const closeFormSubuser = document.querySelector(".close-formSubuser");
 
     function toggleTableVisibility() {
         if (create_subuser.checked) {
-            subusersTable.classList.add("hidden");
+            resultados.classList.add("hidden");
             closeFormGroup.classList.add("hidden");
             closeFormSubuser.classList.remove("hidden");
         } 
         else if (create_candidate.checked) {
-            subusersTable.classList.add("hidden");
+            resultados.classList.add("hidden");
             closeFormSubuser.classList.add("hidden");
             closeFormGroup.classList.remove("hidden");
         }
         else {
-            subusersTable.classList.remove("hidden");
+            resultados.classList.remove("hidden");
             closeFormGroup.classList.add("hidden");
             closeFormSubuser.classList.add("hidden");
         }
